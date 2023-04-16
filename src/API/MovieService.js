6fc88fe0cf8aaf606eec48322ae0c34855480c27
AndreyZaginay <andrey201223@ukr.net>
2023-04-17 01:01:46 +0300

    add castom hook useFetching

--- src/API/MovieService.js
+++ src/API/MovieService.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 export default class MovieService {
+    key;
     constructor() {
         this.key = '2e5de327';
     }
     static async getMovieByTitle(titleName) {
-        const respose = await axios.get(`http://www.omdbapi.com/?t=${titleName}&apikey=${this.key}`);
+        const respose = await axios.get(`http://www.omdbapi.com/?t=${titleName}&apikey=2e5de327`);
         return respose;
     }
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useFetching } from '../hooks/useFetching';
 import MyButton from './UI/button/MyButton';
 import MyInput from './UI/input/MyInput';
+import MovieService from '../API/MovieService';
 
+const Search = ({ callback }) => {
 
-const Search = () => {
+    const [title, setTitle] = useState('');
+
+    // const findMovie = () => {
+    //   const response = MovieService.getMovieByTitle(title);
+        
+    // }
+    
+    const [fetchMovie, isPostsLoading, postError] = useFetching ( async () => {
+        const response = await MovieService.getMovieByTitle(title);
+        console.log(response);
+    })
+
   return (
     <div className='search'>
-        <MyInput/>
-        <MyButton>
+        <MyInput 
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            type="text" 
+            placeholder="Enter a movie name"
+        />
+        <MyButton onClick={fetchMovie}>
             Search
         </MyButton>
     </div>

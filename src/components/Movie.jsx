aeb839998a8e3f69fb75a211f4300f4b285183fb
AndreyZaginay <andrey201223@ukr.net>
2023-04-17 17:36:37 +0300

    made the decomposition of the project

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import Search from './Search'
+import React, { useState } from 'react';
+import MyButton from './UI/button/MyButton';
+import MyInput from './UI/input/MyInput';
+import { useFetching } from '../hooks/useFetching';
+import MovieService from '../API/MovieService';
+import MovieInfo from './MovieInfo';
 
 const Movie = () => {
+
+  const [title, setTitle] = useState('');
+  const [movie, setMovie] = useState(null);
+
+  const [fetchMovie, isMovieLoading, movieError] = useFetching ( async () => {
+      if (!title) {
+        return; 
+      }
+      const response = await MovieService.getMovieByTitle(title);
+      setMovie(response.data);
+  })
+
   return (
     <div className='movie'>
-          <Search/>
+         <div className='search'>
+            <MyInput 
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+              type="text" 
+              placeholder="Enter a movie name"
+            />
+            <MyButton onClick={fetchMovie}>
+              Search
+            </MyButton>
+          </div>
+        <MovieInfo/>
     </div>  
   )
 }

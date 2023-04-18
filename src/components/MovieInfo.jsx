cdc06c9b24e122384237c3f1ecf64d5b04f034de
AndreyZaginay <andrey201223@ukr.net>
2023-04-18 20:29:22 +0300

    add styles to movie info component

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -3,16 +3,23 @@ import React from 'react'
 const MovieInfo = ({ movie }) => {
 
   if (!movie) {
-    return <h1>Enter the movie name</h1>
+    return <h1 style={{color: '#ffb92a', textAlign: 'center'}}>Enter the movie name</h1>
   }
 
   return (
-        <div>
+        <div className='movie-container'>
           {movie.Response === 'True'
-          ? <div>
-            {movie.Title}
-          </div>
-          : <h1>Movie with that name doesn't exist</h1>
+          ? <div className='movie-info'>
+              <img src={movie.Poster} alt="" />
+              <div>  
+                <h1>{movie.Title}</h1>
+                <p className='margin-top-15px'>Rating: {movie.imdbRating}</p>
+                <p className='margin-top-15px'>Released: {movie.Released}</p>
+                <p className='margin-top-15px'>Writer: {movie.Writer}</p>
+                <p className='margin-top-15px'><b>Plot:</b> {movie.Plot}</p>
+              </div>
+            </div>
+          : <h1 style={{color: '#ffb92a', textAlign: 'center'}}>{ movie.Error }</h1>
           }
         </div>
   )
